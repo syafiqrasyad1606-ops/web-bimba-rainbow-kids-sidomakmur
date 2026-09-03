06da9d91434de35d054beaeb6ddf8b01a1bc544d
Syafiq Rasyad <syafiqrasyad1606@gmail.com>
2026-09-03 16:48:46 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles.css'
-import App from './App.jsx'
+import './styles/nav-login.css'
+import Home from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
